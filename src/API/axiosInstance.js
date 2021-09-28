f7cbb2d2ff7dd08d27065c28925afe1df0b252f4
Hala Elsaka <helsaka@qe-software.com>
2021-09-28 14:32:58 +0200

    fix api url

--- src/API/axiosInstance.js
+++ src/API/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://test-api.edfa3ly.io/",
+  baseURL: "https://test-api.edfa3ly.io/",
 });
 
 export default axiosInstance;
